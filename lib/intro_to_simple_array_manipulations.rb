3173663a3df2aca1646f4f267e3058f2287852eb
Olga Rosas <olga.rosas@gmail.com>
2019-06-01 19:43:26 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,45 @@
+def using_push(array, string)
+  array << string
+end
+
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+def using_pop(array)
+  array.pop
+end
+
+def using_pop_with_args(array)
+  array.pop(2)
+end
+
+def using_shift(array)
+  array.shift
+end
+
+def shift_with_args(array)
+  
+end
+
+def using_concat(array1, array2)
+
+end
+
+def using_insert(arrray, element)
+
+end
+
+def using_uniq(array)
+
+end
+
+def using_flatten(array)
+
+end
+
+def using_delete(array, string)
+
+end
+
+def using_delete_at(array, integer)
